49b6188fb229bd761c154f643dd77f323153c220
ClaireSalome <claire.c.salome@gmail.com>
2017-10-19 11:39:40 +0200

    Lancement projectile
    Au clic de la souris, le projectile se lance.
    Vitesse a un impact sur sa trajectoire.
    
    A faire :
    prendre en compte l'angle dans sa trajectoire

--- Assets/Components/Move.cs
+++ Assets/Components/Move.cs
@@ -5,15 +5,11 @@ public class Move : MonoBehaviour {
 	/*
 	 * vitesse à laquelle le projectile est lancé 
 	 */
-	public float vitesse_init = 5.0f ;
+	public float vitesse_init = 8.0f ;
 
 	/*
 	 * angle de la trajectoire
 	 */
 	public int angle = 30;
 
-	/*
-	 * la vitesse de l'objet décroît 
-	 */ 
-	public float speed_decrease = 0.2f;
 }
--- Assets/Systems/MoveProjectile.cs
+++ Assets/Systems/MoveProjectile.cs
@@ -5,7 +5,9 @@ public class MoveProjectile : FSystem {
 
 	// _projectile = famille des entités ayant le composant Move
 	private Family _projectile = FamilyManager.getFamily(new AllOfComponents(typeof(Move)));
-	private int cpt = 0;
+
+	// line going from the origin to some direction
+	private Ray2D projectileToDirection;
 
 	// Use this to update member variables when system pause. 
 	// Advice: avoid to update your families inside this function.
@@ -16,37 +18,37 @@ public class MoveProjectile : FSystem {
 	// Advice: avoid to update your families inside this function.
 	protected override void onResume(int currentFrame){
 	}
+		
 
 	// Use to process your families.
 	protected override void onProcess(int familiesUpdateCount) {
-		
+
 		foreach (GameObject go in _projectile) {
 			Transform tr = go.GetComponent<Transform> ();
 			Move mo = go.GetComponent<Move> ();
+			Rigidbody2D rb = go.GetComponent<Rigidbody2D> ();
 
-			float speed = mo.vitesse_init - (mo.speed_decrease*cpt);
-			int angle = mo.angle;
+			Vector3 pos_init = tr.position;
+			Vector3 mousePoint = Vector3.zero;
 
-			if (speed != 0) {
-				float new_x = speed * Mathf.Cos (angle) * Time.deltaTime;
-
-				float new_y = ((-Physics.gravity.y / 2) *
-				             (Mathf.Pow (new_x, 2) / (Mathf.Pow (speed * Mathf.Cos (angle), 2)))) +
-				             (new_x * Mathf.Tan (angle));
-
-
-				tr.position += new Vector3 (Mathf.Abs (new_x), new_y);
+			if(Input.GetMouseButtonDown(1).Equals(true) ){
+				mousePoint = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+				mousePoint.z = 0f;
+				tr.position = mousePoint ;
 			}
 
-			cpt++;
+			// quand on relache le bouton droit de la souris
+			if (Input.GetMouseButtonUp(1).Equals(true) ){
+				// normalement
+				// devrait créer la bonne direction
+				projectileToDirection = new Ray2D (mousePoint,  pos_init - mousePoint);
 
-			//TODO
-			/*
-			 * QUand on lance un object avec vitesse v et angle alpha, quelle est la distance en fonction du temps ?
-			 * v = d/t -> d = v*t à chaque instant (mouvement uniforme)
-			 * et l'angle ?
-			 * 
-			 */
+				rb.velocity = new Vector2 (mo.vitesse_init, mo.vitesse_init);
+
+				// le projectile est de nouveau soumis aux forces physiques
+				rb.isKinematic = false;
+
+			}
 		}
 
 	}
