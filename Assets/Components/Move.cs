79baab75d31a7a73d28287d07085113ff90450e4
bearojas <bearojas95@gmail.com>
2017-10-22 20:36:25 +0200

    Revert "agrandissement des objets"
    This reverts commit 3add1ccc8127021ca2030069bf1cccffcea0f693.

--- Assets/Components/Move.cs
+++ Assets/Components/Move.cs
@@ -1,13 +1,11 @@
 ﻿using UnityEngine;
 
 public class Move : MonoBehaviour {
-	//composant pour récupérer les entrées du joueur concernant l'angle et la vitesse
-
 
 	/*
 	 * vitesse à laquelle le projectile est lancé 
 	 */
-	public float vitesse = 8.0f ;
+	public float vitesse_init = 8.0f ;
 
 	/*
 	 * angle de la trajectoire
