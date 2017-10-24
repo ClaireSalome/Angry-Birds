47da6521d33d040355411435f47f259a823328f4
bearojas <bearojas95@gmail.com>
2017-10-24 20:21:31 +0200

    changement de la trajectoire

--- Assets/Components/Move.cs
+++ Assets/Components/Move.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
 
 public class Move : MonoBehaviour {
+	//composant pour récupérer les entrées du joueur concernant l'angle et la vitesse
+
 
 	/*
 	 * vitesse à laquelle le projectile est lancé 
 	 */
-	public float vitesse_init = 8.0f ;
+	//public float vitesse = 8.0f ;
+
+	public Vector3 earth_gravity = new Vector3 (0,-9.81f,0);
 
 	/*
 	 * angle de la trajectoire
 	 */
 	public int angle = 30;
+
+	public Vector3 vitesse = new Vector3(2.5f, 4.5f, 0);
 
 }
---
+++ Assets/Systems/MoveProSystem.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using FYFY;
+
+public class MoveProSystem : FSystem {
+	//Système pour soumettre le projectile aux forces 
+
+
+	private Family _projectile = FamilyManager.getFamily(new AllOfComponents(typeof(Move)));
+
+	// Use this to update member variables when system pause. 
+	// Advice: avoid to update your families inside this function.
+	protected override void onPause(int currentFrame) {
+	}
+
+	// Use this to update member variables when system resume.
+	// Advice: avoid to update your families inside this function.
+	protected override void onResume(int currentFrame){
+	}
+
+	// Use to process your families.
+	protected override void onProcess(int familiesUpdateCount) {
+
+		foreach (GameObject go in _projectile) {
+
+			Rigidbody2D rb = go.GetComponent<Rigidbody2D> ();
+			DataProjectile dp = go.GetComponent<DataProjectile> ();
+
+			//au clic gauche de la souris
+			if (Input.GetKey (KeyCode.Mouse0).Equals (true)) {
+				rb.velocity = dp.get_calculated_velocity ();
+				rb.isKinematic = false;
+			}
+				
+
+		}
+	}
+}
