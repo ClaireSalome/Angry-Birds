653fe7047061ed9627b6e56daf2edc6da39a5a23
bearojas <bearojas95@gmail.com>
2017-12-03 23:52:43 +0100

    TP 27/11
    - le projectile roule sur le sol:
    ajout du composant RigidBody non-Kinematic au sol et “freeze” de sa position (pour qu’il ne tombe pas). Ajout du composant Collision Sensitive 2D au projectile.
    - modification du CollisionSystem pour traiter separement les collisions avec le sol/structures et celles avec les récompenses.
    - les structures ne traversent plus le sol: ajout d'un Box collider au lieu de Edge collider

--- Assets/Systems/CollisionSystem.cs
+++ Assets/Systems/CollisionSystem.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using FYFY;
+using FYFY_plugins.CollisionManager ;
 using FYFY_plugins.TriggerManager ;
 
 public class CollisionSystem : FSystem {
 
 
 	private Family _triggered2D = FamilyManager.getFamily(new AllOfComponents(typeof(Triggered2D))) ;
+	private Family _incollision = FamilyManager.getFamily(new AllOfComponents(typeof(InCollision2D)));
 
 	//récupérer le score total pour les récompenses
 	private TotalScore total = GameObject.FindGameObjectWithTag("total").GetComponent<TotalScore>() ;
@@ -13,21 +15,30 @@ public class CollisionSystem : FSystem {
 	// Use to process your families.
 	protected override void onProcess(int familiesUpdateCount) {
 
-		foreach (GameObject go in _triggered2D) {
-			Triggered2D tr = go.GetComponent<Triggered2D> ();
+		// gestion des collisions avec le sol et les structures
+		foreach (GameObject go in _incollision) {
+			InCollision2D col = go.GetComponent<InCollision2D> ();
+
 			Move mo = go.GetComponent<Move> ();
 
-			foreach (GameObject target in tr.Targets) {
+			foreach (GameObject target in col.Targets) {
 				// ground collision
-				//quand le projectile ne bouge plus -> nouveau projectile créé donc ne plus toucher à la vitesse
+				// quand le projectile ne bouge plus -> nouveau projectile créé donc ne plus toucher à la vitesse
 				if (target.tag.Equals ("Ground") && mo.inMovement == true) {
 					mo.groundContact = true;
-					mo.vitesse.y =0f;
+					mo.vitesse.y = 0f;
 				}
 
-				if (target.tag.Equals ("wood_struct")) {
-					mo.vitesse.x = 0f;
-				}
+				//				if (target.tag.Equals ("wood_struct")) {
+				//					mo.vitesse.x = 0f;
+				//				}
+			}
+		}
+
+		// détection des récompenses
+		foreach (GameObject go in _triggered2D) {
+			Triggered2D tr = go.GetComponent<Triggered2D> ();
+			foreach (GameObject target in tr.Targets){
 
 				if (target.tag.Equals ("reward")) {
 					Collect co = target.GetComponent<Collect> ();
