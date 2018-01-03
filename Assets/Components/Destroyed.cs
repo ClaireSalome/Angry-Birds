26c97368e9e6795bf0654118ed8e4230b1549243
ClaireSalome <claire.c.salome@gmail.com>
2018-01-03 19:32:58 +0100

    Structure détruite
    Premier essai  avec le niveau 1

---
+++ Assets/Components/Destroyed.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class Destroyed : MonoBehaviour {
+	// Advice: FYFY component aims to contain only public members (according to Entity-Component-System paradigm).
+
+	public bool destroyed = false ;
+
+}
---
+++ Assets/Components/DestroyedStruct.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class DestroyedStruct : MonoBehaviour {
+	// Advice: FYFY component aims to contain only public members (according to Entity-Component-System paradigm).
+
+	public int nb_destroyed_struct = 0;
+
+}
